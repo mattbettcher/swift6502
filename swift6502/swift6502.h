25500ee2dfa10177f733392d1b2434b90b4598b7
Tony Pan <tony.pan@purestorage.com>
2014-06-10 15:44:34 -0700

    Initial Commit

---
+++ swift6502/swift6502.h
@@ -0,0 +1,19 @@
+//
+//  swift6502.h
+//  swift6502
+//
+//  Created by Tony Pan on 6/10/14.
+//  Copyright (c) 2014 Tony Pan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for swift6502.
+FOUNDATION_EXPORT double swift6502VersionNumber;
+
+//! Project version string for swift6502.
+FOUNDATION_EXPORT const unsigned char swift6502VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift6502/PublicHeader.h>
+
+
